2334d5ba454dc4e8f0ff251ee4a624c60c60d6f0
Dan O'Malley <dan.omalley@amag.com>
2025-01-07 12:36:46 -0500

    cordova printer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BluetoothPrinter } from '@kduma-autoid/capacitor-bluetooth-printer';
+import { Printer } from "@awesome-cordova-plugins/printer";
+
 
 @Component({
   selector: 'app-root',
@@ -14,11 +16,20 @@ export class AppComponent {
   message = ""
   devices: {name: string, address: string}[] = []
   async handleClick() {
+
     if (this.busy) return
     this.busy = true
     this.devices = []
     this.message = "looking for printers"
     try {
+      const x = await Printer.pick()
+      this.message = String(x)
+    } catch(e) {
+      this.message = String(e)
+    }
+
+
+    /*try {
       const data = await BluetoothPrinter.list()
       for (const d of data.devices) {
         this.devices.push({name: d.name, address: d.address})
@@ -26,7 +37,7 @@ export class AppComponent {
       this.message = this.devices.length ? "" : "no devices found"
     } catch(e) {
       this.message = String(e)
-    }
+    }*/
 
 
     this.busy = false
