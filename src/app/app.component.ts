194c2d5b863033ea9373c8553e746d8a162775b3
Dan O'Malley <dan.omalley@amag.com>
2025-01-06 19:37:38 -0500

    add capacitor bluetooth printer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BluetoothPrinter } from '@kduma-autoid/capacitor-bluetooth-printer';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,25 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'capacitor-test';
+  busy = false
+  message = ""
+  devices: {name: string, address: string}[] = []
+  async handleClick() {
+    if (this.busy) return
+    this.busy = true
+    this.devices = []
+    this.message = "looking for printers"
+    try {
+      const data = await BluetoothPrinter.list()
+      for (const d of data.devices) {
+        this.devices.push({name: d.name, address: d.address})
+      }
+      this.message = this.devices.length ? "" : "no devices found"
+    } catch(e) {
+      this.message = String(e)
+    }
+
+
+    this.busy = false
+  }
 }
